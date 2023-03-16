de2a0db21595790e7e19d6ea5a4a674de0476f6d
Akram-hossain12 <akramhossain0502@gmail.com>
2023-03-17 00:36:33 +0600

    login with sgin

--- src/Componentes/LogIn/Login.js
+++ src/Componentes/LogIn/Login.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css'
+
 
 const Login = () => {
     return (
-        <div>
-            
-        </div>
+      
+            <div class="box">
+                <form autocomplete="off">
+                    <h2>Sign in</h2>
+                    <div class="inputBox">
+                        <input name='email' type="email" required="required"/>
+                            <span>Email</span>
+                            <i></i>
+                    </div>
+                    <div class="inputBox">
+                        <input type="password" required="required"/>
+                            <span>Password</span>
+                            <i></i>
+                    </div>
+                    <div class="links">
+                        <a href="#">Forgot Password ?</a>
+                        <Link To="/register">Signup</Link>
+                    </div>
+                    <input type="submit" value="Login"/>
+                </form>
+            </div>
+
     );
 };
 
--- src/Componentes/Register/Register.js
+++ src/Componentes/Register/Register.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            
-        </div>
+        <div class="box">
+        <form autocomplete="off">
+            <h2>Sign up</h2>
+            <div class="inputBox">
+                <input name='name' type="text" required="required"/>
+                    <span>Userame</span>
+                    <i></i>
+            </div>
+            <div class="inputBox">
+                <input name='email' type="email" required="required"/>
+                    <span>Email</span>
+                    <i></i>
+            </div>
+            <div class="inputBox">
+                <input type="password" required="required"/>
+                    <span>Password</span>
+                    <i></i>
+            </div>
+            <div class="links">
+                <a href="#">Forgot Password ?</a>
+                <Link To="/login">Login</Link>
+            </div>
+            <input type="submit" value="Register"/>
+        </form>
+    </div>
     );
 };
 
