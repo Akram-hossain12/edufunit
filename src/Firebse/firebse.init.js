43f1d86ad22603a2b7e00d351ab338cdf1259cdf
Akram-hossain12 <akramhossain0502@gmail.com>
2023-03-27 16:20:49 +0600

    solveError

--- src/Firebse/firebse.init.js
+++ src/Firebse/firebse.init.js
@@ -5,12 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
+  apiKey: "AIzaSyCatWn09ZgNHhO-9x6za76z9o9_hYYH_-E",
   authDomain:process.env.REACT_APP_authDomain,
   projectId:process.env.REACT_APP_projectId,
   storageBucket:process.env.REACT_APP_storageBucket,
   messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,}
+  appId:process.env.REACT_APP_appId,
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
